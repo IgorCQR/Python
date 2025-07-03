a573beb92c23bacce866607449b4ecc976b0e764
Ashey <Igorregatieri9@gmail.com>
2025-07-03 18:53:08 -0300

    pequenas alteracoes

--- Atividades/TempConvert.py
+++ Atividades/TempConvert.py
@@ -2,5 +2,5 @@ print("Conversor de temperatura (celsius para fahrenheit)")
 tempC = float(input("Informe a temperatura (celsius): "))
 
 fahren = (tempC * 1.8) + 32
-print(f"A temperatura de {tempC}°C em fahrenheit é de {fahren} graus!")
+print(f"A temperatura de {tempC}°C em fahrenheit é de {fahren}°F")
 
--- Atividades/calculadora.py
+++ Atividades/calculadora.py
@@ -13,5 +13,5 @@ print(f"A soma entre {Numero} e {Numero2} è: {result1}")
 print(f"A diferença entre {Numero} e {Numero2} è: {result2}")
 print(f"O produto entre {Numero} e {Numero2} è: {result3}")
 print(f"A divisão entre {Numero} e {Numero2} è: {round(result4)} (valor arredondado)") #ROUND arredonda o valor
-print(f"A protência entre {Numero} (base) e {Numero2} (expoente) è: {result5}")
+print(f"A potência entre {Numero} (base) e {Numero2} (expoente) è: {result5}")
 print(f"O resto da divisão entre {Numero} e {Numero2} è: {result6}")
