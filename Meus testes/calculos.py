c41df559404b135cf632393159af024fffe51107
Ashey <Igorregatieri9@gmail.com>
2025-07-04 19:12:56 -0300

    novidades

---
+++ Meus testes/calculos.py
@@ -0,0 +1,9 @@
+import math
+
+a = int(input('Informe outro numero: '))
+c = int(input('Informe outro número: '))
+b = int(input('Informe um numero: '))
+
+delta = pow(b,2) + 4 * a * c 
+
+print('O delta é', delta)
