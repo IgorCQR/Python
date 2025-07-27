2eaf57702c441fd44ad4ebdaa0d237356fb6b62e
Ashey <Igorregatieri9@gmail.com>
2025-07-26 21:34:08 -0300

    Livro fundamentos da programacao
    Pequenos testes/atividades e exemplos tirados do livro Fundamentos da Programação

---
+++ Meus testes/livroFundamentos.py
@@ -0,0 +1,9 @@
+# cont = 0 
+# for i in range(1,101):
+    # cont += i
+    # print(cont, end=' ')
+
+total = 0
+for i in range(2, 1001, 2):
+    total += i
+print(f'Soma dos números pares entre 2 e 1000: {total}')
