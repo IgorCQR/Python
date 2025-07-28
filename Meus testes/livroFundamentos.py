c991c060c48769e19a33f7aeeeb1b9961e2eb8d3
Ashey <Igorregatieri9@gmail.com>
2025-07-27 21:13:01 -0300

    Upade livroFundamentos

--- Meus testes/livroFundamentos.py
+++ Meus testes/livroFundamentos.py
@@ -3,7 +3,14 @@
     # cont += i
     # print(cont, end=' ')
 
-total = 0
-for i in range(2, 1001, 2):
-    total += i
-print(f'Soma dos números pares entre 2 e 1000: {total}')
+# total = 0
+# for i in range(2, 1001, 2):
+    # total += i
+# print(f'Soma dos números pares entre 2 e 1000: {total}')
+
+quanti = 0
+for i in range(1,7):
+    numero = int(input('Informe um numero: '))
+    if numero == 0:
+        quanti += 1
+print(f'Quantidade de zeros digitados: {quanti}')
