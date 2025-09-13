06ee07334c09d6033c640e62d0eeb64bf20a7be5
Ashey <Igorregatieri9@gmail.com>
2025-09-13 16:46:39 -0300

    Trabalho de matematica
    finalizando o traballho de história e aprendendo MATCH

---
+++ Conceitos/case.py
@@ -0,0 +1,6 @@
+#estrutura SWITCH/CASE
+
+variavel = int(input("numero: "))
+match(variavel):
+    case(1):
+        print("resultado")
--- Meus testes/Atividade03.py
+++ Meus testes/Atividade03.py
@@ -3,21 +3,32 @@ print('OPERAÇÃO COM FRAÇÕES')
 numerador1 = int(input('Informe o numerador da primeira fração: '))
 denominador1 = int(input('Informe o denomiador da primeira fração: '))
 
-denominador2 = int(input('Informe o denomiador da segunda fração: '))
 numerador2 = int(input('Informe o numerador da segunda fração: '))
+denominador2 = int(input('Informe o denominador da segunda fração: '))
 
 if numerador1 == 0 or numerador2 == 0:
     print('Valor do numerador incorreto!')
 else:
   opcao = int(input('Opções: \n[1] Soma \n[2] Subtração \n[3] Produto \n[4] Divisão \nEscolha: '))
 
-if opcao == 1:
-   resultado = ((numerador1/denominador1) * denominador2) + ((numerador2/denominador2) * denominador1) 
-elif opcao == 2:
-   resultado = ((numerador1/denominador1) * denominador2) - ((numerador2/denominador2) * denominador1) 
-elif opcao == 3:
-   resultado = (numerador1/denominador1) * (numerador2/denominador2)
-elif opcao == 4:
-   resultado = (numerador1/denominador1) / (numerador2/denominador2)
-
-print(f'O resultado da operação é {resultado:.2f}')
+match(opcao):
+   case(1):
+      if(denominador1 == denominador2):
+         resultado = (numerador1 + numerador2) / denominador1
+         print(f"O resultado é {resultado:.2f} ou {numerador2 + numerador1}/{denominador1}")
+      else:
+         resultado = ((numerador1/denominador1) * denominador2) + ((numerador2/denominador2) * denominador1) 
+         print(f"O resultado é {resultado:.2f} ou {((numerador1*denominador2) + (numerador2*denominador1))}/{denominador1*denominador2}")
+   case(2):
+      if(denominador2 == denominador1):
+         resultado = (numerador1 - numerador2) / denominador1
+         print(f"O resultado é {resultado:.2f} ou {numerador1 - numerador2}/{denominador1}")
+      else:
+         resultado = ((numerador1/denominador1) * denominador2) - ((numerador2/denominador2) * denominador1) 
+         print(f"O resultado é {resultado:.2f} ou {((numerador1*denominador2) - (numerador2*denominador1))}/{denominador1*denominador2}")
+   case(3):
+      resultado = (numerador1 * numerador2) / (denominador1 * denominador2)
+      print(f"O resultado é {resultado:.2f} ou {numerador1*numerador2}/{denominador1*denominador2}") 
+   case(4):
+      resultado = (numerador1 * denominador2) / (denominador1 * numerador1)
+      print(f"O resultado é {resultado:.2f} ou {numerador1*denominador2}/{denominador1*numerador2}") 
